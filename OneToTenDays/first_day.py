a62fdf780beebc46d37662fb904c43cb6a09078b
xwojziarnik <woj.ziarnik@gmail.com>
2022-04-25 20:20:49 +0200

    Second excercise

--- OneToTenDays/first_day.py
+++ OneToTenDays/first_day.py
@@ -7,3 +7,20 @@ for name in names:
     if not isinstance(name, str):
         continue
     print(name)
+
+#----------------------------------------
+
+counter = 0
+number = 2
+prime = []
+
+while counter < 10:
+    for i in range(2, number):
+        if number % i == 0:
+            break
+    else:
+        prime.append(str(number))
+        counter += 1
+    number += 1
+
+print(','.join(prime))
